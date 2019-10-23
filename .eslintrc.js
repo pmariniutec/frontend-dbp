f1bafa7bb1311ce8780856702342b594b691bd58
pieromarini <piero.marini21@gmail.com>
2019-10-23 16:52:28 -0500

    Boilerplate

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,28 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  }
+	root: true,
+	env: {
+		node: true
+	},
+	'extends': [
+		'plugin:vue/recommended',
+		'@vue/standard'
+	],
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'vue/html-indent': ['error', 'tab', {
+			'attribute': 1,
+			'closeBracket': 0,
+			'ignores': []
+		}],
+
+		'no-new': 0,
+		'indent': ['error', 'tab'],
+		'no-tabs': 0,
+		'quotes': [2, 'single'],
+		'one-var': [0, 'never']
+	},
+	parserOptions: {
+		parser: 'babel-eslint'
+	}
 }
